7d788ac511f5f096e2337d8f4e721eaba58cc8b9
Aurelius Prochazka <aure@aure.com>
2017-07-27 09:16:25 -0700

    Booster tests

--- AudioKit/Common/Tests/AKBoosterTests.swift
+++ AudioKit/Common/Tests/AKBoosterTests.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2017 AudioKit. All rights reserved.
 //
 
-import Foundation
+import AudioKit
+import XCTest
+
+class AKBoosterTests: AKTestCase {
+
+    func testDefault() {
+        output = AKBooster(input)
+        AKTestNoEffect()
+    }
+
+    func testParamters() {
+        output = AKBooster(input, gain: 0.1)
+        AKTestMD5("da4225703cdfcf1fc46dfb104d1824b2")
+    }
+
+    func testParamtersSetAfterInit() {
+        let booster = AKBooster(input)
+        booster.gain = 0.1
+        output = booster
+        AKTestMD5("da4225703cdfcf1fc46dfb104d1824b2")
+    }
+
+    func testGain() {
+        output = AKBooster(input, gain: 0.7)
+        AKTestMD5("b6e42381963b1047cb4890e6c0da29f3")
+    }
+
+
+}
